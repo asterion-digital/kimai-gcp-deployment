0b2ce618d4e86c6c65432a553639f5c18cdb035c
James Blair <mail@jamesblair.net>
2021-06-15 17:07:52 +1200

    Split deployment to two pods with service.

--- 4-create-application-deployment.sh
+++ 4-create-application-deployment.sh
@@ -6,4 +6,6 @@ export KUBECONFIG=/home/$USER/.kube/config
 gcloud container clusters get-credentials kimai-gcp --region australia-southeast1
 
 # Apply the application deployment yaml
-kubectl apply -f 4-application-deployment.yaml
+kubectl apply -f 4-app-deployment.yaml
+kubectl apply -f 4-db-deployment.yaml
+kubectl apply -f 4-db-service.yaml
