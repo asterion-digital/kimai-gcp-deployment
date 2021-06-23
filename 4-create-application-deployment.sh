79fc6a508d3271085b225d90b88dfe4b48e01681
James Blair <mail@jamesblair.net>
2021-06-23 17:42:42 +1200

    Use subfolder for yaml config files

--- 4-create-application-deployment.sh
+++ 4-create-application-deployment.sh
@@ -6,7 +6,7 @@ export KUBECONFIG=/home/$USER/.kube/config
 gcloud container clusters get-credentials kimai-gcp --region australia-southeast1
 
 # Apply the application deployment yaml
-kubectl apply -f 4-app-deployment.yaml
-kubectl apply -f 4-db-deployment.yaml
-kubectl apply -f 4-db-service.yaml
-kubectl apply -f 4-app-service.yaml
+kubectl apply -f config/app-deployment.yaml
+kubectl apply -f config/db-deployment.yaml
+kubectl apply -f config/db-service.yaml
+kubectl apply -f config/app-service.yaml
